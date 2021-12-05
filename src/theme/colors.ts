490571a5bdc1555e0b9cace25f65edbfeb5f8e1e
talkcryptome <talkcryptowithme@gmail.com>
2021-12-04 20:46:49 -0700

    First Commit
    First Commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,55 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#00A7E1",
+  primaryBright: "#00A7E1",
+  primaryDark: "#00A7E1",
+  secondary: "#5A3A31",
+  success: "#31D0AA",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#FAF9FA",
+  backgroundDisabled: "#E9EAEB",
+  contrast: "#191326",
+  invertedContrast: "#FFFFFF",
+  input: "#eeeaf4",
+  tertiary: "#EFF4F5",
+  text: "#4059AD",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#585858",
+  borderColor: "#E9EAEB",
+  card: "#FFFFFF",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#5A3A31",
+  background: "#343135",
+  backgroundDisabled: "#3c3742",
+  contrast: "#FFFFFF",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  primaryDark: "#0098A1",
+  tertiary: "#353547",
+  text: "#4059AD",
+  textDisabled: "#666171",
+  textSubtle: "#c9c4d4",
+  borderColor: "#524B63",
+  card: "#27262c",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+  },
+};
